0450bd338eeb30a03f1d179e1d8b052115327fc2
akrjuger <krjuger@ya.ru>
2020-07-23 12:57:48 +0300

    refactor API for future works

--- src/api.js
+++ src/api.js
@@ -1,64 +1,37 @@
 import EventModel from './models/event.js';
 
+const Methods = {
+  GET: `GET`,
+  PUSH: `PUSH`,
+  DELETE: `DELETE`
+};
+
 export default class API {
   constructor(endPoint, authToken) {
     this._endPoint = endPoint;
     this._authToken = authToken;
-
-    this._events = [];
-    this._destionations = [];
-    this._offers = [];
   }
 
   getEvents() {
-    return fetch(this._endPoint + `points`, {
-      method: `GET`,
-      headers: {
-        "Authorization": this._authToken
-      }
-    }).then((response) => {
-      return response.json();
-    })
-    .then((datas) => {
-      datas.forEach((data) => {
-        // console.log(data);
-        this._events.push(new EventModel(data).getEvent());
-      });
-      return this._events;
-      // console.log(this._eventModels.forEach((eventModel) => eventModel.getEvent()));
-    });
+    return this._load(`points`)
+    .then((datas) => datas.map((data) => new EventModel(data).getEvent()));
   }
 
   getDestinations() {
-    return fetch(this._endPoint + `destinations`, {
-      method: `GET`,
-      headers: {
-        "Authorization": this._authToken
-      }
-    }).then((response) => {
-      return response.json();
-    })
-    .then((datas) => {
-      // console.log(datas);
-      return (datas);
-      // console.log(this._eventModels.forEach((eventModel) => eventModel.getEvent()));
-    });
+    return this._load(`destinations`);
   }
 
   getOffers() {
-    return fetch(this._endPoint + `offers`, {
-      method: `GET`,
+    return this._load(`offers`);
+  }
+
+  _load(url, method = Methods.GET) {
+    return fetch(this._endPoint + url, {
+      method,
       headers: {
         "Authorization": this._authToken
       }
-    }).then((response) => {
-      return response.json();
-    })
-    .then((datas) => {
-      return datas;
-      // console.log(datas);
-      // console.log(this._eventModels.forEach((eventModel) => eventModel.getEvent()));
-    });
+    }).then((response) => response.json());
   }
 
 }
